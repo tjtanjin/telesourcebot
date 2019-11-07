7c11b5d53a2bdb569c7df397ba4c24fd9dbd5adb
tjtanjin <cjtanjin@gmail.com>
2019-11-08 02:27:31 +0800

    Added regex.

--- submodules/user_input.py
+++ submodules/user_input.py
@@ -3,7 +3,7 @@ from telegram.ext.dispatcher import run_async
 from submodules import code_executor as ce
 from submodules import user_management as um
 from submodules import logger as lg
-import json, threading, jsbeautifier, os
+import json, threading, jsbeautifier, os, re
 global executing_code
 execute_code = False
 
